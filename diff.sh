45885a1322e82ebc5663ad34792814090a0872fd
AgOH <stagoh17@gmail.com>
2022-08-14 23:11:33 +0800

    feat: add diff script

---
+++ diff.sh
@@ -0,0 +1,10 @@
+set -e
+g++ temp.cpp -o temp -DLOCAL -O2 -Wno-unused-result
+g++ test.cpp -o test -Wno-unused-result
+while true; do
+    python3 data.py >in.in
+    ./temp
+    ./test <in.in >out2.out
+    diff out.out out2.out
+    sleep 0.5
+done
