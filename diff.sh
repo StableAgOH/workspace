684b6aed061a99976e5c537b11930208c2c9ccaf
StableAgOH <stagoh17@gmail.com>
2022-10-18 00:55:51 +0800

    feat: enhance diff.sh

--- diff.sh
+++ diff.sh
@@ -1,7 +1,8 @@
 set -e
 g++ temp.cpp -o temp -DLOCAL -O2 -Wno-unused-result -std=c++20
 g++ test.cpp -o test -Wno-unused-result
-while true; do
+for ((i = 1; ; i++)); do
+    echo "No.$i"
     python3 data.py >in.in
     ./temp
     ./test <in.in >out2.out
