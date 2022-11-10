f2aa5e4bf9540ee638b97740d49d8730c3784da6
StableAgOH <stagoh17@gmail.com>
2022-11-10 10:48:04 +0800

    fix: fix diff script

--- diff.sh
+++ diff.sh
@@ -4,9 +4,9 @@ g++ temp.cpp -o temp -DLOCAL -O2 -Wno-unused-result -std=c++20
 g++ test.cpp -o test -Wno-unused-result
 for ((i = 1; ; i++)); do
     echo "No.$i"
-    python3 data.py >in.in
-    ./temp
-    ./test <in.in >out2.out
-    diff out.out out2.out
+    python3 data.py >testcases/233.in
+    ./temp 233
+    ./test <testcases/233.in >testcases/233.ans
+    diff testcases/233.out testcases/233.ans
     sleep 0.5
 done
