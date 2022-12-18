60d62783bcb45ab2616f94e860239aa09872765d
AgOH <stagoh17@gmail.com>
2022-12-18 17:41:38 +0800

    feat: enhance diff script

--- diff.ps1
+++ diff.ps1
@@ -8,27 +8,30 @@ function ExitIfNotZero {
     }
 }
 
-Write-Output "[INFO] Compiling temp.cpp"
-clang++ temp.cpp -o temp -O2 -std=c++20
-ExitIfNotZero "[CE] temp.cpp"
+$Program1 = $args[0] ? $args[0] : "temp.cpp"
+$Program2 = $args[1] ? $args[1] : "test.cpp"
 
-Write-Output "[INFO] Compiling test.cpp"
-clang++ test.cpp -o test -O2 -std=c++20
-ExitIfNotZero "[CE] test.cpp"
+Write-Output "[INFO] Compiling $Program1"
+clang++ $Program1 -o temp -O2 -std=c++20
+ExitIfNotZero "[CE] $Program1"
+
+Write-Output "[INFO] Compiling $Program2"
+clang++ $Program2 -o test -O2 -std=c++20
+ExitIfNotZero "[CE] $Program2"
 
 for ($i = 1; ; $i++) {
     Write-Output "[INFO] Testcase No.$i"
 
-    python data.py > .\in.in
+    python data.py > in.in
     ExitIfNotZero "[RE] data.py"
 
-    Get-Content in.in | .\temp > .\out.out
-    ExitIfNotZero "[RE] temp.cpp"
+    Get-Content in.in | ./temp > out.out
+    ExitIfNotZero "[RE] $Program1"
 
-    Get-Content in.in | .\test > .\out2.out
-    ExitIfNotZero "[RE] test.cpp"
+    Get-Content in.in | ./test > out2.out
+    ExitIfNotZero "[RE] $Program2"
 
-    fc.exe /W .\out.out .\out2.out
+    fc.exe /W out.out out2.out
     ExitIfNotZero "[ERROR] Two results do not match"
 
     Start-Sleep 0.5
