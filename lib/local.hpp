cd62fdd3e0e4c8b02f088e77790813fd3587e4b5
AgOH <stagoh17@gmail.com>
2022-12-18 15:41:42 +0800

    perf: purge template

--- lib/local.hpp
+++ lib/local.hpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #include "prettyprint.hpp"
 using namespace std;
+
 template <typename Arg1>
 void __cerr(const char* name, Arg1&& arg1) { cerr<<name<<": "<<arg1<<endl; }
 template <typename Arg1, typename... Args>
@@ -10,4 +11,20 @@ void __cerr(const char* names, Arg1&& arg1, Args&&... args)
     cerr.write(names, comma-names)<<": "<<arg1<<" | ";
     while(*++comma==' ');
     __cerr(comma, args...);
+}
+
+chrono::_V2::system_clock::time_point __c1;
+void __before_run()
+{
+    freopen("in.in", "r", stdin);
+#ifndef DEBUG
+    freopen("out.out", "w", stdout);
+#endif
+    __c1 = chrono::high_resolution_clock::now();
+}
+
+void __after_run()
+{
+    auto __c2 = chrono::high_resolution_clock::now();
+    cerr<<"Time Used:"<<chrono::duration_cast<chrono::milliseconds>(__c2-__c1).count()<<"ms"<<endl;
 }
