cbe7cd95fc034008338215a0dacb725fea082ef4
AgOH <stagoh17@gmail.com>
2023-01-06 03:37:57 +0800

    feat: add solver to template

--- lib/local.hpp
+++ lib/local.hpp
@@ -2,29 +2,25 @@
 #include "prettyprint.hpp"
 using namespace std;
 
+#ifdef LOCAL
+    ifstream __fin("in.in");
+    ofstream __fout("out.out");
+#define cin __fin
+#ifndef DEBUG
+#define cout __fout
+#endif
+#endif
+
+namespace local
+{
 template <typename Arg1>
-void __cerr(const char* name, Arg1&& arg1) { cerr<<name<<": "<<arg1<<endl; }
+void debug(const char* name, Arg1&& arg1) { cerr<<name<<": "<<arg1<<endl; }
 template <typename Arg1, typename... Args>
-void __cerr(const char* names, Arg1&& arg1, Args&&... args)
+void debug(const char* names, Arg1&& arg1, Args&&... args)
 {
     auto comma = strchr(names+1, ',');
     cerr.write(names, comma-names)<<": "<<arg1<<" | ";
     while(*++comma==' ');
-    __cerr(comma, args...);
+    debug(comma, args...);
 }
-
-chrono::_V2::system_clock::time_point __c1;
-void __before_run()
-{
-    freopen("in.in", "r", stdin);
-#ifndef DEBUG
-    freopen("out.out", "w", stdout);
-#endif
-    __c1 = chrono::high_resolution_clock::now();
-}
-
-void __after_run()
-{
-    auto __c2 = chrono::high_resolution_clock::now();
-    cerr<<"Time Used:"<<chrono::duration_cast<chrono::milliseconds>(__c2-__c1).count()<<"ms"<<endl;
-}
+} // namespace local
