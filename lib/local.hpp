5cf1df66a948255ee6283d64ca298b7e104e7e00
StableAgOH <stagoh17@gmail.com>
2023-01-06 06:00:48 +0800

    feat: enhance lib

--- lib/local.hpp
+++ lib/local.hpp
@@ -5,12 +5,12 @@ using namespace std;
 namespace local
 {
 template <typename Arg1>
-void debug(const char* name, Arg1&& arg1) { cerr<<name<<": "<<arg1<<endl; }
+void debug(const char* name, Arg1&& arg1) { clog<<name<<": "<<arg1<<endl; }
 template <typename Arg1, typename... Args>
 void debug(const char* names, Arg1&& arg1, Args&&... args)
 {
     auto comma = strchr(names+1, ',');
-    cerr.write(names, comma-names)<<": "<<arg1<<" | ";
+    clog.write(names, comma-names)<<": "<<arg1<<" | ";
     while(*++comma==' ');
     debug(comma, args...);
 }
