15148d06f3b01a979d9949872fc35819f47dd29c
StableAgOH <stagoh17@gmail.com>
2023-02-06 22:44:01 +0800

    feat: update template

--- lib/local.hpp
+++ lib/local.hpp
@@ -12,3 +12,18 @@ void debug(const char* names, Arg1&& arg1, Args&&... args)
     while(*++comma==' ');
     debug(comma, args...);
 }
+
+class timer
+{
+private:
+    static chrono::_V2::system_clock::time_point begin_time;
+public:
+    static void set_begin_time() { begin_time = chrono::high_resolution_clock::now(); }
+    static void utime(int line)
+    {
+        auto end_time = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::milliseconds>(end_time-begin_time);
+        clog<<duration.count()<<" ms used at line "<<line<<endl;
+    }
+};
+chrono::_V2::system_clock::time_point timer::begin_time;
