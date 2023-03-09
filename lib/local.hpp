288cc6213e11ce2e57f2de860dcb874caf346ee2
StableAgOH <stagoh@qq.com>
2023-03-09 16:14:45 +0800

    feat: use sharkdp/dbg-macro

--- lib/local.hpp
+++ lib/local.hpp
@@ -1,18 +1,7 @@
 #include <bits/stdc++.h>
-#include "prettyprint.hpp"
+#include "dbg-macro/dbg.h"
+#define debug(...) dbg(__VA_ARGS__)
 using namespace std;
-
-template <typename Arg1>
-void debug(const char* name, Arg1&& arg1) { clog<<name<<": "<<arg1<<endl; }
-template <typename Arg1, typename... Args>
-void debug(const char* names, Arg1&& arg1, Args&&... args)
-{
-    auto comma = strchr(names+1, ',');
-    clog.write(names, comma-names)<<": "<<arg1<<" | ";
-    while(*++comma==' ');
-    debug(comma, args...);
-}
-
 class timer
 {
 private:
