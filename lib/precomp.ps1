a57cf1a96c4e6e95bdbf3a903199f8a1fcd8bcd5
AgOH <stagoh17@gmail.com>
2023-03-20 14:27:33 +0800

    feat&refactor: add precompile

---
+++ lib/precomp.ps1
@@ -0,0 +1,8 @@
+$file = "local.h"
+$std = "c++20"
+
+mkdir -Path "$file.gch" -Force
+g++ $file -o "$file.gch/$file-$std.gch" -std="$std"
+g++ $file -o "$file.gch/$file-$std-g.gch" -std="$std" -g
+g++ $file -o "$file.gch/$file-$std-O2.gch" -std="$std" -O2
+g++ $file -o "$file.gch/$file-$std-g-O2.gch" -std="$std" -g -O2
