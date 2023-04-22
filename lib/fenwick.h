e549355ff4922de7a1b491d6fb247d556567b4e8
StableAgOH <stagoh@qq.com>
2023-04-22 23:11:30 +0800

    perf: enhance fenwick

--- lib/fenwick.h
+++ lib/fenwick.h
@@ -2,17 +2,22 @@ template <typename T>
 class fenwick
 {
 private:
-    int n;
-    vector<T> c;
-    int lowbit(int x) { return x&-x; }
+    size_t n;
+    vector<T> pre;
+    size_t lowbit(size_t x) { return x&-x; }
 public:
-    fenwick(const int n) : n(n) { c.resize(n+1); }
-    void add(int p, const T& x) { for(auto i=p;i<=n;i+=lowbit(i)) c[p] += x; }
-    T query(int p)
+    fenwick(const size_t n) : n(n) { pre.resize(n+1); }
+    void add(size_t p, const T& x) { for(auto i=p;i<=n;i+=lowbit(i)) pre[p] += x; }
+    T query(size_t p)
     {
+        assert(p<=n);
         T sum = T();
-        for(auto i=p;i;i-=lowbit(i)) sum += c[i];
+        for(auto i=p;i;i-=lowbit(i)) sum += pre[i];
         return sum;
     }
-    T query(int l, int r) { return query(r)-query(l-1); }
+    T query(size_t l, size_t r)
+    {
+        assert(l<=r&&r<=n);
+        return query(r)-query(l-1);
+    }
 };
