040eef0017051982b8751e93735af5c54900007a
AgOH <stagoh17@gmail.com>
2023-11-18 13:45:58 +0800

    Fix prefix_sum implementation and code snippet indentation

--- lib/prefix_sum.h
+++ lib/prefix_sum.h
@@ -9,8 +9,9 @@ private:
 public:
     prefix_sum(const ranges::range auto& r)
     {
-        pre.resize(ranges::size(r));
-        partial_sum(ranges::begin(r), ranges::end(r), pre.begin(), op);
+        pre.reserve(ranges::size(r));
+        T sum = T();
+        for(auto& i : r) pre.push_back(sum = op(sum, i));
     }
     T operator()(int p) { return p>=0?pre[p]:T(); }
     T operator()(int l,int r) { return operator()(r)-operator()(l-1); }
