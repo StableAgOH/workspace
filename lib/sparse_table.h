517dba47f3eb922523e374c99422342f0f2b4836
AgOH <stagoh17@gmail.com>
2023-11-27 20:50:47 +0800

    Add sparse_table data structure implementation

---
+++ lib/sparse_table.h
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <typename T, auto O, size_t L=(size_t)ceil(log2(1e7))>
+class sparse_table
+{
+private:
+    array<vector<T>, L> f;
+public:
+    sparse_table(const ranges::range auto& r)
+    {
+        for(auto i : r) f[0].emplace_back(i);
+        int n = f[0].size();
+        for(int i=1;(1<<i)<=n;i++)
+            for(int j=0;j+(1<<i)-1<n;j++)
+                f[i].emplace_back(O(f[i-1][j], f[i-1][j+(1<<(i-1))]));
+    }
+    T operator()(int l,int r)
+    {
+        int k = log2(r-l+1);
+        return O(f[k][l], f[k][r-(1<<k)+1]);
+    }
+};
