df9a5c8c6b903333fa6ff3d8f0c426c7b6e25ce6
AgOH <stagoh17@gmail.com>
2024-11-08 11:33:40 +0800

    删除 prefix_sum，重构 string_hasher

--- lib/string_hasher.h
+++ lib/string_hasher.h
@@ -2,21 +2,15 @@ class string_hasher
 {
 private:
     int mod;
-    vector<ll> ppow, hpre;
+    vector<ll> w, h;
 public:
-    string_hasher(const string& s, int p=114514, int mod=998244353) : mod(mod)
+    string_hasher(string_view s, int p=23333, int mod=998244353) : mod(mod), w(1,1), h(1)
     {
-        ppow.push_back(1);
-        hpre.push_back(0);
         for(size_t i=1;i<=s.length();i++)
         {
-            ppow.push_back(ppow[i-1]*p%mod);
-            hpre.push_back(hpre[i-1]*p%mod+s[i-1]%mod);
+            w.push_back(w[i-1]*p%mod);
+            h.push_back((h[i-1]*p+s[i-1])%mod);
         }
     }
-    int get(int l,int r)
-    {
-        assert(l<=r);
-        return (hpre[r]-hpre[l-1]*ppow[r-l+1]%mod+mod)%mod;
-    }
+    auto operator()(int l,int r) { return (h[r]-h[l-1]*w[r-l+1]%mod+mod)%mod; }
 };
