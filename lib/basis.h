700309c10351379ae820687e48d5c60d4ca6ff24
AgOH <stagoh17@gmail.com>
2024-11-22 10:31:19 +0800

    feat(basis): create

---
+++ lib/basis.h
@@ -0,0 +1,28 @@
+template <integral T>
+struct basis
+{
+    vector<T> base;
+    basis() = default;
+    basis(ranges::range auto&& r)
+    {
+        for(auto i : r) insert(i);
+    }
+    void insert(T x)
+    {
+        for(auto i : base) x = min(x, x^i);
+        for(auto& i : base) i = min(i, i^x);
+        if(x) base.push_back(x);
+    }
+    bool can_construct(T x)
+    {
+        for(auto i : base) x = min(x, x^i);
+        return !x;
+    }
+    T get_max()
+    {
+        T ret = 0;
+        for(auto i : base) ret = max(ret, ret^i);
+        return ret;
+    }
+    T get_min() { return base.back(); }
+};
