3f04d77f9f2904b9f1818afeeb9bf28d4f6ef4f1
AgOH <stagoh17@gmail.com>
2024-11-24 15:22:52 +0800

    refactor(dsu): remove siz property

--- lib/dsu.h
+++ lib/dsu.h
@@ -1,18 +1,15 @@
 class dsu
 {
-private:
-    vector<int> fa, siz;
+    vector<int> fa;
     int find(int x) { return fa[x]==x?x:fa[x]=find(fa[x]); }
 public:
-    dsu(int n) : fa(n), siz(n,1) { iota(fa.begin(), fa.end(), 0); }
+    dsu(int n) : fa(n) { iota(fa.begin(), fa.end(), 0); }
     bool merge(int x,int y)
     {
         x=find(x), y=find(y);
         if(x==y) return false;
         fa[x] = y;
-        siz[y] += siz[x];
         return true;
     }
     int operator[](int x) { return find(x); }
-    int size(int x) { return siz[x]; }
 };
