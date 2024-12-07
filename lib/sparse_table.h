e4d9bbf9226eafbb567a7bf42538803926ae6a17
AgOH <stagoh17@gmail.com>
2024-12-07 15:24:22 +0800

    refactor(sparse_table): enhance sparse_table implementation

--- lib/sparse_table.h
+++ lib/sparse_table.h
@@ -1,20 +1,43 @@
-template <typename T, typename O, size_t L=(size_t)ceil(log2(1e7))>
+/**
+ * @brief ST 表，支持所有“可重复贡献问题”。参见：https://oi-wiki.org/ds/sparse-table/
+ * 
+ * 构造（以在 a 上建立一个求 max 的 ST 表为例）：
+ * @code{.cpp} sparse_table<int, [](int x,int y) { return max(x,y); }> st(a); @endcode
+ * 
+ * @tparam T 元素类型
+ * @tparam Op 可重复贡献二元操作（max、min、gcd、lcm、bit_and、bit_or 等），建议使用 lambda 表达式
+ */
+template <typename T, auto Op>
 class sparse_table
 {
-private:
-    array<vector<T>, L> f;
+    vector<vector<T>> f;
 public:
-    sparse_table(const ranges::range auto& r)
+    /**
+     * @param r 要建立 ST 表的序列
+     */
+    sparse_table(ranges::range auto&& r)
     {
-        for(auto i : r) f[0].emplace_back(i);
-        int n = f[0].size();
-        for(int i=1;(1<<i)<=n;i++)
-            for(int j=0;j+(1<<i)-1<n;j++)
-                f[i].emplace_back(O()(f[i-1][j], f[i-1][j+(1<<(i-1))]));
+        int n = ranges::size(r);
+        f.emplace_back(ranges::begin(r), ranges::end(r));
+        for(int i=1;(1<<i)<n;i++)
+        {
+            f.emplace_back(n);
+            for(int j=1<<i;j<n;j+=1<<(i+1))
+            {
+                f[i][j-1] = f[0][j-1];
+                for(int k=j-2;k>=j-(1<<i);k--) f[i][k] = Op(f[0][k], f[i][k+1]);
+                f[i][j] = f[0][j];
+                for(int k=j+1;k<min(n,j+(1<<i));k++) f[i][k] = Op(f[i][k-1], f[0][k]);
+            }
+        }
     }
+    /**
+     * @brief 查询闭区间 [l,r] 的结果
+     */
     T operator()(int l,int r)
     {
-        int k = log2(r-l+1);
-        return O()(f[k][l], f[k][r-(1<<k)+1]);
+        if(l==r) return f[0][l];
+        int p = bit_width(unsigned(l^r))-1;
+        return Op(f[p][l], f[p][r]);
     }
 };
