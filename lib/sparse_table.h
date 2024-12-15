658ca9d0a9733c01747727f88b932de38bc508f8
AgOH <stagoh17@gmail.com>
2024-12-15 18:57:07 +0800

    feat(sparse_table): enhance constructor to accept custom operation function

--- lib/sparse_table.h
+++ lib/sparse_table.h
@@ -1,28 +1,32 @@
-template <typename T, auto Op>
+template <typename T>
 class sparse_table
 {
+    using CR = const T&;
+    function<T(T,T)> op;
     vector<vector<T>> f;
-public:
-    sparse_table(ranges::range auto&& r)
+    void build(ranges::range auto&& rg)
     {
-        int n = ranges::size(r);
-        f.emplace_back(ranges::begin(r), ranges::end(r));
+        int n = ranges::size(rg);
+        f.emplace_back(ranges::begin(rg), ranges::end(rg));
         for(int i=1;(1<<i)<n;i++)
         {
             f.emplace_back(n);
             for(int j=1<<i;j<n;j+=1<<(i+1))
             {
                 f[i][j-1] = f[0][j-1];
-                for(int k=j-2;k>=j-(1<<i);k--) f[i][k] = Op(f[0][k], f[i][k+1]);
+                for(int k=j-2;k>=j-(1<<i);k--) f[i][k] = op(f[0][k], f[i][k+1]);
                 f[i][j] = f[0][j];
-                for(int k=j+1;k<min(n,j+(1<<i));k++) f[i][k] = Op(f[i][k-1], f[0][k]);
+                for(int k=j+1;k<min(n,j+(1<<i));k++) f[i][k] = op(f[i][k-1], f[0][k]);
             }
         }
     }
-    T operator()(int l,int r)
+public:
+    sparse_table(ranges::range auto&& rg, function<T(T,T)> op) : op(op) { build(rg); }
+    sparse_table(ranges::range auto&& rg, CR (*op)(CR,CR)) : op(op) { build(rg); }
+    T operator()(int l,int32_t r)
     {
         if(l==r) return f[0][l];
         int p = bit_width(unsigned(l^r))-1;
-        return Op(f[p][l], f[p][r]);
+        return op(f[p][l], f[p][r]);
     }
 };
