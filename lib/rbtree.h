d0e5d361717222283bc2ac59b5b8805c4200a94b
AgOH <stagoh17@gmail.com>
2025-01-08 14:18:39 +0800

    refactor(rbtree): simplify implementation by inheriting from base tree class

--- lib/rbtree.h
+++ lib/rbtree.h
@@ -1,23 +1,20 @@
 #include <ext/pb_ds/assoc_container.hpp>
 using namespace __gnu_pbds;
 template <typename T>
-class rbtree
+using rbtree_base = tree<pair<T,int>, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T>
+class rbtree : public rbtree_base<T>
 {
-    int id = 0;
-    tree<pair<T, int>, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update> t;
+    int cnt = 0;
 public:
-    auto begin() { return t.begin(); }
-    auto end() { return t.end(); }
-    auto size() { return t.size(); }
-    auto empty() { return t.empty(); }
-    auto insert(const T& x) { t.insert({x, id++}); }
-    auto erase(const T& x) { t.erase(t.lower_bound({x, INT_MIN})); }
-    auto order_of_key(const T& x) { return t.order_of_key({x, INT_MIN})+1; }
-    auto find_by_order(int rk) { return t.find_by_order(rk-1); }
-    auto prev(const T& x)
+    auto insert(const T& x) { rbtree_base<T>::insert({x, cnt++}); }
+    auto erase(const T& x)
     {
-        auto it = t.lower_bound({x, INT_MIN});
-        return it==t.begin()?t.end():--it;
+        auto it = lower_bound(x);
+        if(it->first==x) rbtree_base<T>::erase(it);
     }
-    auto next(const T& x) { return t.upper_bound({x, INT_MAX}); }
+    auto order_of_key(const T& x) const { return rbtree_base<T>::order_of_key({x, INT_MIN}); }
+    auto find_by_order(int rk) const { return rbtree_base<T>::find_by_order(rk); }
+    auto lower_bound(const T& x) const { return rbtree_base<T>::lower_bound({x, INT_MIN}); }
+    auto upper_bound(const T& x) const { return rbtree_base<T>::upper_bound({x, INT_MAX}); }
 };
