f9848fcc96d71cbf428f7d249afad6c90cca7ef0
AgOH <stagoh17@gmail.com>
2025-01-15 15:28:08 +0800

    refactor(fenwick): simplify implementation and improve variable naming for clarity

--- lib/fenwick.h
+++ lib/fenwick.h
@@ -1,32 +1,26 @@
-template <typename T, typename O=plus<T>>
+template <typename T>
 class fenwick
 {
     static constexpr auto lowbit(auto x) { return x&-x; }
-    int n;
-    vector<T> c;
+    size_t n;
+    vector<T> data;
 public:
-    fenwick(int n) : n(n), c(n+1) {}
-    fenwick(const ranges::range auto& r) : fenwick(ranges::size(r))
+    fenwick(ranges::range auto&& rg) : n(ranges::size(rg)), data(n+1)
     {
         vector<T> p(n+1);
-        partial_sum(r.begin(), r.end(), p.begin()+1);
-        for(int i=1;i<=n;i++) c[i] = p[i]-p[i-lowbit(i)];
+        inclusive_scan(rg.begin(), rg.end(), p.begin()+1);
+        for(size_t i=1;i<=n;i++) data[i] = p[i]-p[i-lowbit(i)];
     }
-    fenwick(int n, const T& x) : fenwick(vector(n,x)) {}
-    void clear() { ranges::fill(c, T()); }
-    int size() { return n; }
-    void add(int p, const T& x)
-    {
-        for(int i=p+1;i<=n;i+=lowbit(i))
-            c[i] = O()(c[i], x);
-    }
-    auto prefix_sum(int p) const
+    fenwick(size_t n, const T& init={}) : fenwick(vector(n, init)) {}
+    auto prefix_sum(size_t p) const
     {
         T sum{};
-        for(int i=p+1;i;i-=lowbit(i))
-            sum = O()(sum, c[i]);
+        for(size_t i=p+1;i;i-=lowbit(i)) sum += data[i];
         return sum;
     }
-    auto operator()(int l) const { return operator()(l,l); }
-    auto operator()(int l, int r) const { return prefix_sum(r)-prefix_sum(l-1); }
+    auto operator()(size_t l) const { return (*this)(l,l); }
+    auto operator()(size_t l, size_t r) const { return prefix_sum(r)-prefix_sum(l-1); }
+    void add(size_t p, const T& x) { for(size_t i=p+1;i<=n;i+=lowbit(i)) data[i] += x; }
+    void clear() { ranges::fill(data, T()); }
+    auto size() const { return n; }
 };
