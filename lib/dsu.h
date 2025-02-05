0aa438db0f092b8ddaaecb80bf88bdf71623950b
AgOH <stagoh17@gmail.com>
2025-02-05 17:06:57 +0800

    feat(dsu): refactored with reference to ac-library

--- lib/dsu.h
+++ lib/dsu.h
@@ -1,15 +1,37 @@
+template <bool KeepMergeOrder=false>
 class dsu
 {
-    vector<int> fa;
-    int find(int x) { return fa[x]==x?x:fa[x]=find(fa[x]); }
+    int n = 0;
+    vector<int> data;
 public:
-    dsu(int n) : fa(n) { iota(fa.begin(), fa.end(), 0); }
-    bool merge(int x,int y)
+    dsu() = default;
+    explicit dsu(int n) : n(n), data(n, -1) {}
+    int leader(int x) { return data[x]<0?x:data[x]=leader(data[x]); }
+    int merge(int x, int y)
     {
-        x=find(x), y=find(y);
-        if(x==y) return false;
-        fa[x] = y;
-        return true;
+        x = leader(x);
+        y = leader(y);
+        if(x==y) return x;
+        if constexpr (!KeepMergeOrder)
+            if(-data[x]<-data[y])
+                swap(x,y);
+        data[x] += data[y];
+        data[y] = x;
+        return x;
     }
-    int operator[](int x) { return find(x); }
+    bool same(int x, int y) { return leader(x)==leader(y); }
+    int size(int x) { return -data[leader(x)]; }
+    auto groups()
+    {
+        vector<int> leader_buf(n), group_size(n);
+        for(int i=0;i<n;i++)
+        {
+            leader_buf[i] = leader(i);
+            group_size[leader_buf[i]]++;
+        }
+        vector<vector<int>> result(n);
+        for(int i=0;i<n;i++) result[leader_buf[i]].push_back(i);
+        erase_if(result, [&](const auto& v) { return v.empty(); });
+        return result;
+    }
 };
