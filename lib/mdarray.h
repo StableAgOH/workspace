d9e2b9df4ee75a3d7d55f8d8ce8ef974159f3379
AgOH <stagoh17@gmail.com>
2025-02-05 15:32:41 +0800

    feat(mdarray): add multi-dimensional array implementation and update code snippet

---
+++ lib/mdarray.h
@@ -0,0 +1,6 @@
+template <typename T, size_t S, size_t... Sizes>
+struct mdarray_helper { using type = std::array<typename mdarray_helper<T, Sizes...>::type, S>; };
+template <typename T, size_t L>
+struct mdarray_helper<T, L> { using type = std::array<T, L>; };
+template <typename T, size_t... Sizes>
+using mdarray = typename mdarray_helper<T, Sizes...>::type;
