5bdf818ae2a625ae3682639d70435da172531a6e
AgOH <stagoh17@gmail.com>
2025-03-26 10:58:33 +0800

    refactor(fenwick): improve data handling and rename methods for clarity

--- lib/ds/fenwick.h
+++ lib/ds/fenwick.h
@@ -1,26 +1,30 @@
 template <typename T>
 class fenwick
 {
-    static constexpr auto lowbit(auto x) { return x&-x; }
-    size_t n;
-    vector<T> data;
 public:
-    fenwick(ranges::range auto&& rg) : n(ranges::size(rg)), data(n+1)
+    fenwick(ranges::range auto&& rg) : data(ranges::size(rg)+1)
     {
-        vector<T> p(n+1);
+        vector<T> p(data.size());
         inclusive_scan(rg.begin(), rg.end(), p.begin()+1);
-        for(size_t i=1;i<=n;i++) data[i] = p[i]-p[i-lowbit(i)];
+        for(size_t i=1;i<p.size();i++) data[i] = p[i]-p[i-lowbit(i)];
     }
     fenwick(size_t n, const T& init={}) : fenwick(vector(n, init)) {}
-    auto prefix_sum(size_t p) const
+    auto scan(size_t p) const
     {
         T sum{};
         for(size_t i=p+1;i;i-=lowbit(i)) sum += data[i];
         return sum;
     }
-    auto operator()(size_t l) const { return (*this)(l,l); }
-    auto operator()(size_t l, size_t r) const { return prefix_sum(r)-prefix_sum(l-1); }
-    void add(size_t p, const T& x) { for(size_t i=p+1;i<=n;i+=lowbit(i)) data[i] += x; }
+    auto operator()(size_t l, size_t r) const { return scan(r)-scan(l-1); }
+    auto operator()(size_t l) const { return (*this)(l, l); }
+    void add(size_t p, const T& x)
+    {
+        for(size_t i=p+1;i<=size();i+=lowbit(i))
+            data[i] += x;
+    }
     void clear() { ranges::fill(data, T()); }
-    auto size() const { return n; }
+    auto size() const { return data.size()-1; }
+private:
+    static constexpr auto lowbit(auto x) { return x&-x; }
+    vector<T> data;
 };
