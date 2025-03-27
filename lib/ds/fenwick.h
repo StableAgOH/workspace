f7294d3bb48fce242be76d7a4752321e3ea24104
AgOH <stagoh17@gmail.com>
2025-03-27 11:33:48 +0800

    refactor(fenwick): rename scan method to prefix and add suffix method for clarity

--- lib/ds/fenwick.h
+++ lib/ds/fenwick.h
@@ -9,13 +9,14 @@ public:
         for(size_t i=1;i<p.size();i++) data[i] = p[i]-p[i-lowbit(i)];
     }
     fenwick(size_t n, const T& init={}) : fenwick(vector(n, init)) {}
-    auto scan(size_t p) const
+    auto prefix(size_t p) const
     {
         T sum{};
         for(size_t i=p+1;i;i-=lowbit(i)) sum += data[i];
         return sum;
     }
-    auto operator()(size_t l, size_t r) const { return scan(r)-scan(l-1); }
+    auto suffix(size_t p) const { return prefix(size()-1)-prefix(p); }
+    auto operator()(size_t l, size_t r) const { return prefix(r)-prefix(l-1); }
     auto operator()(size_t l) const { return (*this)(l, l); }
     void add(size_t p, const T& x)
     {
