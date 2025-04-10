963c782c4fb5bb9e4ca0d19fb88be8dd4e966681
AgOH <stagoh17@gmail.com>
2025-04-10 11:47:44 +0800

    refactor(fenwick): simplify clear method initialization

--- lib/ds/fenwick.h
+++ lib/ds/fenwick.h
@@ -20,6 +20,6 @@ public:
     auto operator()(size_t l, size_t r) const { return scan(r)-scan(l-1); }
     auto operator()(size_t l) const { return (*this)(l, l); }
     void add(size_t p, const T& x) { for(size_t i=p+1;i<=size();i+=lowbit(i)) data[i] += x; }
-    void clear() { ranges::fill(data, T()); }
+    void clear() { data.assign(data.size(), {}); }
     auto size() const { return data.size()-1; }
 };
