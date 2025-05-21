23a75c114e9f92a3ee383608abf17a3474153107
AgOH <stagoh17@gmail.com>
2025-05-21 11:19:30 +0800

    fix(difference_constraint): add explicit keyword to constructor and specify return type for operator()

--- lib/graph/difference_constraint.h
+++ lib/graph/difference_constraint.h
@@ -5,11 +5,11 @@ class difference_constraint
     vector<tuple<int, int, T>> edges;
     void add_arc(int u, int v, T w) { edges.emplace_back(u, v, w); }
 public:
-    difference_constraint(int n) : n(n) { for(int i=0;i<n;i++) add_arc(n, i, 0); }
+    explicit difference_constraint(int n) : n(n) { for(int i=0;i<n;i++) add_arc(n, i, 0); }
     void add_leq(int u, int v, T w) { add_arc(v, u, w); }
     void add_geq(int u, int v, T w) { add_arc(u, v, -w); }
     void add_eq(int u, int v, T w) { add_leq(u, v, w); add_geq(u, v, w); }
-    auto operator()()
+    vector<T> operator()()
     {
         vector<T> dis(n+1, numeric_limits<T>::max()/2);
         dis[n] = 0;
@@ -27,6 +27,6 @@ public:
             }
             if(!relax) break;
         }
-        return relax?vector<T>{}:dis;
+        return relax?{}:dis;
     }
 };
