7fdaac8731af81f69460ee799294b9d27426ba8d
AgOH <stagoh17@gmail.com>
2025-05-23 11:31:39 +0800

    refactor(sqrt_decomposition): simplify constructor and set methods, improve block handling

--- lib/ds/sqrt_decomposition.h
+++ lib/ds/sqrt_decomposition.h
@@ -1,33 +1,44 @@
-template <typename T, auto Op, auto InvOp>
+template <typename T, auto Op, auto InvOp=nullopt>
 class sqrt_decomposition
 {
     static_assert(convertible_to<invoke_result_t<decltype(Op), T, T>, T>);
-    static_assert(convertible_to<invoke_result_t<decltype(InvOp), T, T>, T>);
-    size_t n, bs;
+    size_t n, sz;
     vector<size_t> id;
     vector<T> data, block;
 public:
     sqrt_decomposition(ranges::range auto&& rg) :
-        n(ranges::size(rg)), bs(sqrt(n)), id(n), data(ranges::begin(rg), ranges::end(rg))
+        n(ranges::size(rg)), sz(sqrt(n)), id(n), data(ranges::begin(rg), ranges::end(rg)), block((n-1)/sz+1)
     {
-        for(size_t i=0;i<n;i++) id[i] = i/bs;
-        block.resize(id[n-1]+1);
-        for(size_t i=0;i<n;i++) block[id[i]] = i%bs==0?data[i]:Op(block[id[i]], data[i]);
+        for(size_t i=0;i<n;i++) id[i] = i/sz;
+        for(size_t i=0;i<n;i++) block[id[i]] = i%sz==0?data[i]:Op(block[id[i]], data[i]);
     }
     sqrt_decomposition(size_t n, const T& init={}) : sqrt_decomposition(vector(n, init)) {}
     void set(size_t p, const T& x)
     {
-        block[id[p]] = InvOp(block[id[p]], data[p]);
-        data[p] = x;
-        block[id[p]] = Op(block[id[p]], data[p]);
+        if constexpr(same_as<decltype(InvOp), nullopt_t>)
+        {
+            data[p] = x;
+            p = id[p];
+            for(auto i=p*sz;i<min(n, (p+1)*sz);i++) block[p] = i%sz==0?data[i]:Op(block[p], data[i]);
+        }
+        else
+        {
+            block[id[p]] = Op(InvOp(block[id[p]], data[p]), x);
+            data[p] = x;
+        }
     }
-    void transform(size_t p, auto&& f) { set(p, f((*this)[p])); }
+    void set(size_t p, const T& x, auto&& f)
+    {
+        static_assert(convertible_to<invoke_result_t<decltype(f), T>, T>);
+        data[p] = x;
+        block[id[p]] = f(block[id[p]]);
+    }
     auto operator[](size_t p) const { return data[p]; }
     auto operator()(size_t l, size_t r) const
     {
         if(id[l]==id[r]) return reduce(data.begin()+l+1, data.begin()+r+1, data[l], Op);
-        auto res = reduce(data.begin()+l+1, data.begin()+bs*(id[l]+1), data[l], Op);
-        res = reduce(data.begin()+bs*id[r], data.begin()+r+1, res, Op);
-        return reduce(block.begin()+id[l]+1, block.begin()+id[r], res, Op);
+        auto res = l%sz==0?block[id[l]]:reduce(data.begin()+l+1, data.begin()+sz*(id[l]+1), data[l], Op);
+        res = reduce(block.begin()+id[l]+1, block.begin()+id[r], res, Op);
+        return reduce(data.begin()+sz*id[r], data.begin()+r+1, res, Op);
     }
 };
