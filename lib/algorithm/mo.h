40deba467e4bbc7e79bcf9bc25781d05f9070269
AgOH <stagoh17@gmail.com>
2025-05-23 11:31:28 +0800

    refactor(mo): update template type and improve query handling

--- lib/algorithm/mo.h
+++ lib/algorithm/mo.h
@@ -1,34 +1,33 @@
-template <typename T>
+template <typename Q>
 class mo
 {
-    int n;
-    vector<pair<int, int>> q;
+    size_t n;
+    vector<Q> q;
 public:
-    mo(int n) : n(n) {}
-    void add_query(int l, int r) { q.emplace_back(l, r); }
-    template <typename C>
-    auto operator()(C&& calculator)
+    mo(size_t n) : n(n) {}
+    void add_query(Q&& q) { this->q.push_back(q); }
+    auto operator()(auto&& push_front, auto&& pop_front, auto&& push_back, auto&& pop_back, auto&& calc)
     {
-        vector<T> ans(q.size());
-        int siz = n/sqrt(q.size())+1;
+        vector<invoke_result_t<decltype(calc), Q>> ans(q.size());
+        int siz = n/sqrt(q.size());
         vector<int> pos(n+1);
         for(int i=1;i<=n;i++) pos[i] = i/siz;
         vector<int> o(q.size());
         iota(o.begin(), o.end(), 0);
         sort(o.begin(), o.end(), [&](int i, int j) {
-            if(pos[q[i].first]!=pos[q[j].first]) return q[i].first<q[j].first;
-            if((pos[q[i].first]&1)) return q[i].second<q[j].second;
-            return q[i].second>q[j].second;
+            if(pos[get<0>(q[i])]!=pos[get<0>(q[j])]) return get<0>(q[i])<get<0>(q[j]);
+            if((pos[get<0>(q[i])]&1)) return get<1>(q[i])<get<1>(q[j]);
+            return get<1>(q[i])>get<1>(q[j]);
         });
         int cl=0, cr=-1;
         for(auto i : o)
         {
-            auto [l,r] = q[i];
-            while(cl>l) calculator.push_front(--cl);
-            while(cr<r) calculator.push_back(++cr);
-            while(cl<l) calculator.pop_front(cl++);
-            while(cr>r) calculator.pop_back(cr--);
-            ans[i] = calculator(l, r);
+            int l=get<0>(q[i]), r=get<1>(q[i]);
+            while(cl>l) push_front(--cl);
+            while(cr<r) push_back(++cr);
+            while(cl<l) pop_front(cl++);
+            while(cr>r) pop_back(cr--);
+            ans[i] = calc(q[i]);
         }
         return ans;
     }
