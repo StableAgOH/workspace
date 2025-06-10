e3a7d568e36bb6f2854923462db132cf2a72791b
AgOH <stagoh17@gmail.com>
2025-06-10 11:17:51 +0800

    refactor(dsu): rename leader and merge methods to operator() and unite for clarity

--- lib/ds/dsu.h
+++ lib/ds/dsu.h
@@ -1,27 +1,23 @@
-template <bool KeepMergeOrder=false>
 class dsu
 {
     vector<int> data;
 public:
     explicit dsu(int n) : data(n, -1) {}
-    int leader(int x) { return data[x]<0?x:data[x]=leader(data[x]); }
-    int merge(int x, int y)
+    int operator()(int x) { return data[x]<0?x:data[x]=(*this)(data[x]); }
+    int unite(int x, int y)
     {
-        x=leader(x), y=leader(y);
+        x=(*this)(x), y=(*this)(y);
         if(x==y) return x;
-        if constexpr (!KeepMergeOrder)
-            if(-data[x]<-data[y])
-                swap(x, y);
+        if(-data[x]<-data[y]) swap(x, y);
         data[x] += data[y];
         data[y] = x;
         return x;
     }
-    bool same(int x, int y) { return leader(x)==leader(y); }
-    size_t size(int x) { return -data[leader(x)]; }
+    int size(int x) { return -data[(*this)(x)]; }
     auto groups()
     {
         vector<vector<int>> result(data.size());
-        for(size_t i=0;i<data.size();i++) result[leader(i)].push_back(i);
+        for(size_t i=0;i<data.size();i++) result[(*this)(i)].push_back(i);
         return result;
     }
 };
