85065e8e17674a271d377400cd6e1eb99e32ac33
AgOH <stagoh17@gmail.com>
2025-06-11 18:58:17 +0800

    refactor(fenwick): rewrite

--- lib/ds/fenwick.h
+++ lib/ds/fenwick.h
@@ -1,25 +1,27 @@
 template <typename T>
 class fenwick
 {
-    static constexpr auto lowbit(auto x) { return x&-x; }
+    static constexpr auto lowbit(size_t x) { return x&-x; }
     vector<T> data;
-    auto scan(size_t p) const
-    {
-        T sum{};
-        for(size_t i=p+1;i;i-=lowbit(i)) sum += data[i];
-        return sum;
-    }
 public:
-    fenwick(ranges::range auto&& rg) : data(ranges::size(rg)+1)
+    fenwick(ranges::range auto&& rg) : data(ranges::begin(rg), ranges::end(rg))
     {
-        vector<T> p(data.size());
-        inclusive_scan(rg.begin(), rg.end(), p.begin()+1);
-        for(size_t i=1;i<p.size();i++) data[i] = p[i]-p[i-lowbit(i)];
+        for(size_t i=1;i<=data.size();i++)
+        {
+            auto j = i+lowbit(i);
+            if(j<=data.size()) data[j-1] += data[i-1];
+        }
     }
     fenwick(size_t n, const T& init={}) : fenwick(vector(n, init)) {}
-    auto operator()(size_t l, size_t r) const { return scan(r)-scan(l-1); }
+    void add(size_t p, const T& x) { for(auto i=p+1;i<=data.size();i+=lowbit(i)) data[i-1] += x; }
+    template <typename U=T>
+    auto operator()(size_t l, size_t r, U init={}) const
+    {
+        for(auto i=r+1;i;i-=lowbit(i)) init += data[i-1];
+        for(auto i=l;i;i-=lowbit(i)) init -= data[i-1];
+        return init;
+    }
     auto operator[](size_t p) const { return (*this)(p, p); }
-    void add(size_t p, const T& x) { for(size_t i=p+1;i<=size();i+=lowbit(i)) data[i] += x; }
     void clear() { data.assign(data.size(), {}); }
-    auto size() const { return data.size()-1; }
+    auto size() const { return data.size(); }
 };
