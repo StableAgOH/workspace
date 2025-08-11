3974cfbbdcf834dd0e2a78fb88a12289c5da11a0
AgOH <stagoh17@gmail.com>
2025-08-11 21:47:44 +0800

    refactor: replace operator() with operator[] for improved element access in dsu class

--- lib/ds/dsu.h
+++ lib/ds/dsu.h
@@ -3,21 +3,22 @@ class dsu
     vector<int> data;
 public:
     explicit dsu(int n) : data(n, -1) {}
-    int operator()(int x) { return data[x]<0?x:data[x]=(*this)(data[x]); }
+    int operator[](int x) { return data[x]<0?x:data[x]=(*this)[data[x]]; }
     int unite(int x, int y)
     {
-        x=(*this)(x), y=(*this)(y);
+        x=(*this)[x], y=(*this)[y];
         if(x==y) return x;
         if(-data[x]<-data[y]) swap(x, y);
         data[x] += data[y];
         data[y] = x;
         return x;
     }
-    int size(int x) { return -data[(*this)(x)]; }
+    int size(int x) { return -data[(*this)[x]]; }
     auto groups()
     {
         vector<vector<int>> result(data.size());
-        for(size_t i=0;i<data.size();i++) result[(*this)(i)].push_back(i);
+        for(size_t i=0;i<data.size();i++) result[(*this)[i]].push_back(i);
+        erase_if(result, &vector<int>::empty);
         return result;
     }
 };
