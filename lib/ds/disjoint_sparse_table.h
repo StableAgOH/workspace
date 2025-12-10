a8ad8d4555f084e7d588409eef2cfa8713e1ef20
AgOH <stagoh17@gmail.com>
2025-12-10 11:18:24 +0800

    feat: split sparse_table

---
+++ lib/ds/disjoint_sparse_table.h
@@ -0,0 +1,28 @@
+template <typename T, auto Op>
+requires convertible_to<invoke_result_t<decltype(Op), T, T>, T>
+class disjoint_sparse_table
+{
+    vector<vector<T>> data;
+public:
+    explicit disjoint_sparse_table(ranges::range auto&& rg)
+    {
+        int n = ranges::size(rg);
+        data.emplace_back(ranges::begin(rg), ranges::end(rg));
+        for(int i=1;(1<<i)<n;i++)
+        {
+            data.emplace_back(n);
+            for(int m=1<<i;m<n;m+=1<<(i+1))
+            {
+                data[i][m-1] = data[0][m-1];
+                for(int j=m-2;j>=m-(1<<i);j--) data[i][j] = Op(data[0][j], data[i][j+1]);
+                data[i][m] = data[0][m];
+                for(int j=m+1;j<min(n, m+(1<<i));j++) data[i][j] = Op(data[i][j-1], data[0][j]);
+            }
+        }
+    }
+    auto operator()(size_t l, size_t r) const
+    {
+        int k = max(0, (int)bit_width(l^r)-1);
+        return Op(data[k][l], data[k][r]);
+    }
+};
--- lib/ds/sparse_table.h
+++ lib/ds/sparse_table.h
@@ -4,25 +4,20 @@ class sparse_table
 {
     vector<vector<T>> data;
 public:
-    sparse_table(ranges::range auto&& rg)
+    explicit sparse_table(ranges::range auto&& rg)
     {
         int n = ranges::size(rg);
         data.emplace_back(ranges::begin(rg), ranges::end(rg));
         for(int i=1;(1<<i)<n;i++)
         {
-            data.emplace_back(n);
-            for(int m=1<<i;m<n;m+=1<<(i+1))
-            {
-                data[i][m-1] = data[0][m-1];
-                for(int j=m-2;j>=m-(1<<i);j--) data[i][j] = Op(data[0][j], data[i][j+1]);
-                data[i][m] = data[0][m];
-                for(int j=m+1;j<min(n, m+(1<<i));j++) data[i][j] = Op(data[i][j-1], data[0][j]);
-            }
+            data.emplace_back(n-(1<<i)+1);
+            for(int j=0;j+(1<<i)-1<n;j++)
+                data[i][j] = Op(data[i-1][j], data[i-1][j+(1<<(i-1))]);
         }
     }
     auto operator()(size_t l, size_t r) const
     {
-        int p = max(0, (int)bit_width(l^r)-1);
-        return Op(data[p][l], data[p][r]);
+        auto k = bit_width(r-l+1)-1;
+        return Op(data[k][l], data[k][r-(1<<k)+1]);
     }
 };
